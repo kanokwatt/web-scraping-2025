46630f07fab228e81da6b692df6f886bab3603f5
KANOKWATT SHIANGJEN <cmuthailand@hotmail.com>
2025-08-20 07:57:07 +0000

    Modify Dataset

--- app.py
+++ app.py
@@ -4,8 +4,8 @@ import pandas as pd
 st.write("สวัสดี กนกวรรธน์")
 
 df = pd.DataFrame({
-  'First Column':  [1, 2, 3, 4],
-  'Second Column': [10, 20, 30, 40] 
+  'First Column':  [1, 2, 3, 4, 5],
+  'Second Column': [10, 20, 30, 40, 50] 
 })
 
 df
